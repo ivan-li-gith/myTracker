975bdacebaea12347983a17437274abc5c9f846c
ivan-li-gith <ivanli9355@gmail.com>
2026-04-06 12:39:21 -0700

    chore: added comments for config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # define env variables with type
     DATABASE_URL: str
     API_USER: str
     API_PASS: str
     GEMINI_API_KEY: str
 
+    # tell it to read from .env file
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8"
