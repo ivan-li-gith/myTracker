bd94dfa0417e99fd6c365cd92fbffacd25a808cc
ivan-li-gith <ivanli9355@gmail.com>
2026-04-07 16:48:53 -0700

    feat: added authorization basic header

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -4,7 +4,17 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
  * A helper function for making requests to the backend API (the routes)
  */
 export async function apiFetch(path: string, options: RequestInit = {}) {
-  const res = await fetch(`${API_URL}${path}`, options);
+  const user = process.env.NEXT_PUBLIC_API_USER;
+  const pass = process.env.NEXT_PUBLIC_API_PASS;
+  const encoded = btoa(`${user}:${pass}`);
+
+  const headers = {
+    ...options.headers,
+    "Authorization": `Basic ${encoded}`,
+  };
+
+  const res = await fetch(`${API_URL}${path}`, {...options, headers});
+
   if (!res.ok) throw new Error(`API error: ${res.status}`);
   return res.json();
 }
